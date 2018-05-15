20ed2b83bb39a5c76100ac1d980aaa794e158fb5
zul <zul@bw30.com>
2018-05-15 14:43:02 +0800

    项目初始化

---
+++ src/main/java/com/redbean/springresearch/SpringResearchApplication.java
@@ -0,0 +1,12 @@
+package com.redbean.springresearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResearchApplication.class, args);
+	}
+}
